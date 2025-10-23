57751d93a1a44ceda1d97adc607bccd2ea279b94
nepesenka <lme20042004@gmail.com>
2025-10-23 17:28:38 +0300

    Enhance calculator with loop and zero division check
    Added a loop for repeated calculations and zero check.

--- by_nepesenka.java
+++ by_nepesenka.java
@@ -16,7 +16,31 @@ public class by_nepesenka {
      float res3 = num1 * num2; System.out.print(res3 + " умножал "); // умножает
      float res4 = num1 / num2; System.out.print(res4 + " делил "); // разделяет
 
-        System.out.println("Получилось: " );
-        System.out.println(res1 + "\n" + res2 + "\n" + res3 + "\n" + res4);
+        System.out.println("Итоги: " );
+        System.out.println(res1 + "\n" + res2 + "\n" + res3 + "\n" + res4); 
+        if (num1 == 0 || num2 == 0) {
+            System.out.println("на ноль делить нельзя");
+        
+            
+        }
+          for (int i = 0; i < 1000000; i++) {
+     System.out.print("Введи 1 число: ");
+     num1 = scan.nextFloat();
+     System.out.print("Введи 2 число: ");
+     num2 = scan.nextFloat();
+
+     res1 = num1 + num2; System.out.print(res1 + " плюсовал "); // плюсует
+     res2 = num1 - num2; System.out.print(res2 + " минусовал "); // минусует
+     res3 = num1 * num2; System.out.print(res3 + " умножал "); // умножает
+     res4 = num1 / num2; System.out.print(res4 + " делил "); // разделяет
+
+        System.out.println("Итоги: " );
+        System.out.println(res1 + "\n" + res2 + "\n" + res3 + "\n" + res4); 
+        if (num1 == 0 || num2 == 0) {
+            System.out.println("на ноль делить нельзя");
+            break;
+        }
+
+    }
     }
 }
